2e4ced650baa654678cae5c6144cbf965d928363
adrien-gtd <adrienadrien0606@gmail.com>
2024-02-10 18:27:30 +0100

    Added a new page: IPP league

--- src/Page/IPPT.tsx
+++ src/Page/IPPT.tsx
@@ -1,7 +1,48 @@
+import Styles from "./css/IPPT.module.css";
+import CarouselData from "../data/CarouselIPPTData";
+import Carousel from "../Components/Carousel";
+import TopSectionImage from "../Assests/IPPT/IPPT_Photo9.jpg";
+
 const IPPT = () => {
   return (
-    <div>IPPT</div>
-  )
-}
+    <div className={Styles.pageContainer}>
+      <div className={Styles.topSection}>
+        <img src={TopSectionImage} alt="...." className={Styles.image} />
+        <div className={Styles.text}>
+          <div className="title">IPP Tournament</div>
+          <div className="subtitle">10ème édition</div>
+        </div>
+      </div>
 
-export default IPPT
+      <div className={Styles.eventDescriptionSection}>
+        <div className={Styles.carrouselContainer}>
+          <Carousel
+            imgs={CarouselData.imgs}
+            descriptions={CarouselData.descriptions}
+            headers={CarouselData.headers}
+          />
+        </div>
+        <div className={Styles.description}>
+          <div className={Styles.title}>Description de l'événement</div>
+          <div className={Styles.text}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque at
+            possimus, repellat qui placeat maiores nam eligendi molestias, natus
+            modi aut amet explicabo, blanditiis odit dolor sunt fugit
+            consectetur? Earum? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Consequatur quo possimus qui officia nam cumque
+            adipisci, alias veniam labore iste molestiae ratione esse, fuga
+            quis, ducimus non corrupti? Incidunt, error!
+          </div>
+        </div>
+      </div>
+
+      <div className={Styles.rulesSections}></div>
+
+      <div className={Styles.inscriptionForm}></div>
+
+      <div className={Styles.contactSection}></div>
+    </div>
+  );
+};
+
+export default IPPT;
---
+++ src/data/CarouselIPPTData.ts
@@ -0,0 +1,14 @@
+import Image1 from "../Assests/IPPT/IPPT_Photo1.jpg"
+import Image2 from "../Assests/IPPT/IPPT_Photo2.jpg"
+import Image3 from "../Assests/IPPT/IPPT_Photo3.jpg"
+import Image4 from "../Assests/IPPT/IPPT_Photo4.jpg"
+import Image5 from "../Assests/IPPT/IPPT_Photo5.jpg"
+import Image6 from "../Assests/IPPT/IPPT_Photo6.jpg"
+import Image7 from "../Assests/IPPT/IPPT_Photo7.jpg"
+import Image8 from "../Assests/IPPT/IPPT_Photo8.jpg"
+
+export default {
+    imgs: [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8],
+    headers: ["", "", "", "", "", "", "", ""],
+    descriptions: ["", "", "", "", "", "", "", ""]
+}
