691e8038858995d6983b8dd437bdafd1a6829829
adrien-gtd <adrienadrien0606@gmail.com>
2024-05-27 15:24:26 +0200

    Started CSSL page

--- src/Components/SponsorIcon.tsx
+++ src/Components/SponsorIcon.tsx
@@ -1,21 +1,29 @@
 import Styles from "./css/SponsorIcon.module.css";
 
 interface Props {
-    image: string;
-    title: string;
-    description: string;
-    buttonName: string;
-    link: string;
-  }
-
-const SponsorIcon = ({ image, title, description, buttonName, link }: Props) => {
-  return (
-    <div className={Styles.cardBody}>
-        <div className={Styles.imgContainer}>
-            <img src={image} alt="..." className={Styles.sponsorImage}/>
-        </div>
-    </div>
-  )
+  image: string;
+  title: string;
+  description: string;
+  buttonName: string;
+  link: string;
 }
 
-export default SponsorIcon
+const SponsorIcon = ({
+  image,
+  title,
+  description,
+  buttonName,
+  link,
+}: Props) => {
+  return (
+    <div className={Styles.cardBody}>
+      <div className={Styles.imgContainer}>
+        <a href={link}>
+          <img src={image} alt="..." className={Styles.sponsorImage} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SponsorIcon;
--- src/Page/CSSL.tsx
+++ src/Page/CSSL.tsx
@@ -1,7 +1,61 @@
+import Countdown from "../Components/Countdown";
+import Styles from "./css/CSSL.module.css"
+
 const CSSL = () => {
   return (
-    <div>CSSL</div>
-  )
-}
+    <div className={Styles.pageContainer}>
+      <div className={Styles.header}>
+        <div className={Styles.title}>Counter-Strike Student League <p/>1ère édition</div>
+        <div className={Styles.countdown}>
+          <Countdown
+            title="La grande final à Telemcom!"
+            year={2024}
+            month={7}
+            day={25}
+            hour={12}
+          />
+        </div>
+      </div>
 
-export default CSSL
+      <div className={Styles.body}>
+        <div className={Styles.rules}>
+          <div className={Styles.title}>Déroulement de l'événement:</div>
+          <div className={Styles.content}>
+            <div className={Styles.pool}>
+              <div className={Styles.title}>Phase de pool:</div>
+              <div className={Styles.text}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+                velit accusantium voluptas, veritatis iure mollitia consequuntur
+                nesciunt adipisci maxime ratione ullam explicabo accusamus sed
+                perferendis, exercitationem vitae quia minima pariatur.
+              </div>
+            </div>
+            <div className={Styles.playoff}>
+              <div className={Styles.title}>Play off:</div>
+              <div className={Styles.text}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                error recusandae beatae ullam suscipit explicabo esse labore
+                consectetur! At modi autem aspernatur quos quia placeat sequi
+                excepturi impedit consectetur animi?
+              </div>
+            </div>
+            <div className={Styles.cashPrize}>
+              <div className={Styles.title}>Cash prize!</div>
+              <div className={Styles.text}>
+                Pour les trois meilleur équipes, un cash prize de 2000€ sera
+                réparti
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={Styles.contact}>
+          Pour toutes question sur l'événement ou pour nous contacter
+          directement, rendez-vous sur le <a href="#">discrod de la CSSL </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CSSL;
