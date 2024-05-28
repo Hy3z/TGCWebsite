1beb6161fe99d513510f0cb610ed07720d899c4e
adrien-gtd <adrienadrien0606@gmail.com>
2024-05-28 16:23:06 +0200

    fix: IPPT topsection fixed

--- src/Page/IPPT.tsx
+++ src/Page/IPPT.tsx
@@ -2,23 +2,25 @@ import Styles from "./css/IPPT.module.css";
 import CarouselData from "../data/CarouselIPPTData";
 import Carousel from "../Components/Carousel";
 import TopSectionImage from "../Assests/IPPT/IPPT_Photo9.jpg";
-import LolLogo from "../Assests/game/lol.png"
-import SsbuLogo from "../Assests/game/ssbublanc.png"
-
+import LolLogo from "../Assests/game/lol.png";
+import SsbuLogo from "../Assests/game/ssbublanc.png";
 
 const IPPT = () => {
-
   return (
     <div className={Styles.pageContainer}>
       <div className={Styles.topSection}>
         <img src={TopSectionImage} alt="...." className={Styles.image} />
         <div className={Styles.text}>
-          <div className="title">IPP Tournament</div>
-          <div className="subtitle">10ème édition</div>
-          <div className="subtitle">6-7 avril 2024</div>
+          <div className={Styles.title}>
+            IPP Tournament
+            <p />
+            10ème édition
+            <p />
+            6-7 avril 2024
+          </div>
           <div className={Styles.imagecontainer}>
-            <img src={LolLogo} alt ="...." />
-            <img src={SsbuLogo} alt ="...." />
+            <img src={LolLogo} alt="...." />
+            <img src={SsbuLogo} alt="...." />
           </div>
         </div>
       </div>
@@ -34,36 +36,46 @@ const IPPT = () => {
         <div className={Styles.description}>
           <div className={Styles.title}>L'IP Paris e-sport Tournament</div>
           <div className={Styles.text}>
-          Le tournoi e-sport organisé par le Telecom Gaming Club est le plus gros tournoi du plateau de Saclay.
-          Ce dernier permet aux joueurs des écoles de l’Institut Polytechnique de Paris et des écoles du plateau de s’affronter sur tout un week-end sur les jeux League of Legends et Smash Bros Ultimate. 
-          Chaque année passée, le tournoi se déroule dans l’enceinte de l’école Télécom Paris.
-
+            Le tournoi e-sport organisé par le Telecom Gaming Club est le plus
+            gros tournoi du plateau de Saclay. Ce dernier permet aux joueurs des
+            écoles de l’Institut Polytechnique de Paris et des écoles du plateau
+            de s’affronter sur tout un week-end sur les jeux League of Legends
+            et Smash Bros Ultimate. Chaque année passée, le tournoi se déroule
+            dans l’enceinte de l’école Télécom Paris.
           </div>
         </div>
       </div>
 
       <div className={Styles.statSection}>
-
         <div className={Styles.statBubble}>
-        <p><strong>100</strong> participants des écoles d'ingénieurs du plateau</p>
+          <p>
+            <strong>100</strong> participants des écoles d'ingénieurs du plateau
+          </p>
         </div>
 
         <div className={Styles.statBubble}>
-        <p><strong>2</strong> jours de compétitions</p>
+          <p>
+            <strong>2</strong> jours de compétitions
+          </p>
         </div>
 
         <div className={Styles.statBubble}>
-        <p><strong>2</strong> jeux</p>
-        </div>
-        
-        <div className={Styles.statBubble}>
-        <p><strong>15 000</strong> minutes visionnées sur Twitch</p>
+          <p>
+            <strong>2</strong> jeux
+          </p>
         </div>
 
         <div className={Styles.statBubble}>
-        <p><strong>400</strong> spectateurs uniques sur Twitch</p>
+          <p>
+            <strong>15 000</strong> minutes visionnées sur Twitch
+          </p>
         </div>
 
+        <div className={Styles.statBubble}>
+          <p>
+            <strong>400</strong> spectateurs uniques sur Twitch
+          </p>
+        </div>
       </div>
 
       <div className={Styles.rulesSections}></div>
